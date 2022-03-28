6e7114efca4bb4abb4e705e70cdc36d2ee3b6dfa
FormoSeanIap <55405280+FormoSeanIap@users.noreply.github.com>
2022-03-28 19:53:18 +0800

    express init

---
+++ app.js
@@ -0,0 +1,23 @@
+// require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// API routes
+
+// Page not found
+app.use(function (req, res, next) {
+    res.status(404).send('page not found');
+});
+
+// Error handling
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.listen(3000);
